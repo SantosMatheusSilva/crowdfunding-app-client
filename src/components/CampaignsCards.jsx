057bac73d91e22d1d5cba69391481c57996e84a6
SantosMatheusSilva <mathsantos52@gmail.com>
2024-09-17 16:18:39 +0100

    CampaignsCards: loading ui

--- src/components/CampaignsCards.jsx
+++ src/components/CampaignsCards.jsx
@@ -44,7 +44,37 @@ function CampaignsCard () {
 
     console.log("campaigns --->", campaigns);
 
-    if(loading) return <p>Loading...</p>
+    // improve loading component ui.
+    if (loading) {
+      return (
+        <div className="flex items-center justify-center h-screen">
+          <div className="flex flex-col items-center space-y-4">
+            <svg
+              className="animate-spin h-12 w-12 text-sky-500"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 4.418 3.582 8 8 8v-4c-2.764 0-5.236-1.122-7.071-2.929z"
+              ></path>
+            </svg>
+            <p className="text-gray-500">Loading...</p>
+          </div>
+        </div>
+      );
+    }
+  
     if(error) return <p>Error: {error.message}</p>
 
 
